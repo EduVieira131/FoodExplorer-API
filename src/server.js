d5adf5155cef9a091dc0c6d537d8afea1aa4844e
EduViera131 <eduviera369@gmail.com>
2023-09-01 15:22:26 -0300

    :feat: initial route setup created

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const AppError = require("./utils/AppError")
+const routes = require('./routes')
 
 const app = express()
+
+app.use(express.json())
+app.use(routes)
 
 app.use((error, res) => {
   if(error instanceof AppError) {
