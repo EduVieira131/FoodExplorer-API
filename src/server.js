50e5556eac8d28caeeb938f4c2dcdf8ab0f00f41
EduViera131 <eduviera369@gmail.com>
2023-09-01 14:42:26 -0300

    :feat: created exception handling middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const AppError = require("./utils/AppError")
 
 const app = express()
+
+app.use((error, res) => {
+  if(error instanceof AppError) {
+    return res.statusCode(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  return res.statusCode(500).json({
+    status:"error",
+    message: error.message
+  })
+})
+
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
