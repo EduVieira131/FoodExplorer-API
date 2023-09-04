747c65f661f22a1aa1af81a4691d9abde0cf654b
EduViera131 <eduviera369@gmail.com>
2023-09-04 16:05:49 -0300

    :test: user creation test created

---
+++ src/services/UserCreateService.spec.js
@@ -0,0 +1,42 @@
+const UserCreateService = require("../services/UserCreateService")
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError")
+
+describe('UserCreateService', () => {
+  let userRepositoryInMemory = null
+  let userCreateService = null
+
+  beforeEach(async () => {
+    userRepositoryInMemory = new UserRepositoryInMemory()
+    userCreateService = new UserCreateService(userRepositoryInMemory)
+  })
+
+  it('user should be created', async () => {
+    const user = {
+      name: "User test",
+      email: "user@test.com",
+      password: "123"
+    }
+
+    const userCreated = await userCreateService.execute(user)
+
+    expect(userCreated).toHaveProperty("id")
+  });
+
+  it("user shouldn't be created with an email that already exists", async () => {
+    const user1 =  {
+      name: "User test 1",
+      email: "user@test.com",
+      password: "123"
+    }
+
+    const user2 =  {
+      name: "User test 2",
+      email: "user@test.com",
+      password: "123"
+    }
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Esse e-mail já está cadastrado."))
+  });
+});
