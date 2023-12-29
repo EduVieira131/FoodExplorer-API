fa1fc37bb03783faffe12c3e6146ccc08cbd4852
EduViera131 <eduviera369@gmail.com>
2023-12-29 10:11:42 -0300

    :feat: authentication middleware created

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
 
+const ensureAuthenticationMiddleware = require('../middlewares/ensureAuthentication')
+
 const productsRouter = Router()
 const ProductsController = require('../controllers/productsController')
 
 const productsController = new ProductsController()
+
+productsRouter.use(ensureAuthenticationMiddleware)
 
 productsRouter.post('/', productsController.create)
 productsRouter.get('/', productsController.index)
