7bebfbf008504be0190ebfd452f126a3b2b01ac6
EduViera131 <eduviera369@gmail.com>
2024-01-06 13:14:05 -0300

    :fix: password length validation implemented

--- src/services/userServices/UserCreateService.js
+++ src/services/userServices/UserCreateService.js
@@ -7,6 +7,12 @@ class UserCreateService {
   }
 
   async execute({ name, email, password, isAdmin }) {
+    if(password.lenght < 6 ) {
+      throw new AppError('A senha deve conter 6 ou mais caracteres.')
+    }
+
+    
+
     const validateUserExistence = await this.userRepository.findByEmail(email)
 
     if (validateUserExistence) {
