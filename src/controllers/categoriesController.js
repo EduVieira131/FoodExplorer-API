087d5f8b6435062705d554dd10d205b1537f8b39
EduViera131 <eduviera369@gmail.com>
2024-02-08 17:23:05 -0300

    :fix: Fix path to category repository error

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,17 +1,17 @@
-const CategoriesRepository = require('../repositories/categoriesRepository/categoriesRepository')
-const CategoriesIndexService = require('../services/categoriesServices/CategoriesIndexService')
+const CategoriesRepository = require("../repositories/categoriesRepository/CategoriesRepository");
+const CategoriesIndexService = require("../services/categoriesServices/CategoriesIndexService");
 
 class categoriesController {
   async index(req, res) {
-    const categoriesRepository = new CategoriesRepository()
+    const categoriesRepository = new CategoriesRepository();
     const categoriesIndexService = new CategoriesIndexService(
       categoriesRepository
-    )
+    );
 
-    const categories = await categoriesIndexService.execute()
+    const categories = await categoriesIndexService.execute();
 
-    return res.status(200).json(categories)
+    return res.status(200).json(categories);
   }
 }
 
-module.exports = categoriesController
+module.exports = categoriesController;
